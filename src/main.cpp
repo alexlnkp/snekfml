443b9c1a2ff2924fec3c0526f9816007cb99e47d
Alex Murkoff <79400603+alexlnkp@users.noreply.github.com>
2024-04-25 18:13:39 +0700

    quick improvement for graphicdebug

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #include "game.hpp"
 
 int main(int argc, char **argv) {
-    
+    // A lifehack so the output is a bit faster for graphical debug.
+    // Adds 64 bytes to the size (only for the Release config for some reason)
+    GRDEBUG(std::ios_base::sync_with_stdio(false))
+
     // If no arguments provided - use pseudo-random seed
     // Else - use user-provided seed
     Snek game(argc > 1 ? std::stoi(argv[1]) : time(__null));
